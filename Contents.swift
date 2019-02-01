74cf8fe5b1a8f6e4ad2be30206e5add937a92e2e
Alumno <alumno@iMac-de-IT-Admin.local>
2019-01-31 21:22:16 -0600

    inital commit

---
+++ Contents.swift
@@ -0,0 +1,9 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+var v1 = "esto es swift"
+let c1 = 4
+var cadena:String = String()
+cadena = "Hola mundo" + v1
+print("El contenido es: \(cadena)")
+print("El contenido es: \(c1)")
